d0667711fefaa56ef68d7f0eb9535dd339ea171e
Benedikt Becsi <benedikt.becsi@boku.ac.at>
2023-04-21 17:33:05 +0200

    updated script open_models_CMIP6.py

--- src/open_models_CMIP6.py
+++ src/open_models_CMIP6.py
@@ -11,3 +11,48 @@ import glob
 import numpy as np
 import xarray as xr
 
+# open and clean up lists for file import
+infiles = open(file="/metstor_nfs/projects/VTreasures/CMIP6_Data/All_tas_metstor.txt", mode="rt")
+files = []
+for line in infiles:
+    files.append(line)
+files = [x.replace("./", "/metstor_nfs/projects/VTreasures/CMIP6_Data/") for x in files]
+files = [x.replace("\n","") for x in files]
+
+for i, l in enumerate(files):
+    if "ssp119" in l:
+        print("found ya!" +l)
+        del(files[i])
+
+hist_files = []
+for i, l in enumerate (files):
+    if "historical" in l:
+        print("found historical file. Add to other list: "+l)
+        hist_files.append(l)
+        del(files[i])
+
+for i, l in enumerate(files):
+    if not "Monthly" in l:
+        print("Error in line "+l)
+        del(files[i])
+
+for i, l in enumerate(hist_files):
+    if not "Monthly" in l:
+        print("Error in line "+l)
+        del(hist_files[i])
+
+for f in hist_files:
+    try:
+        f1 = xr.open_dataset(f)
+        print("successfully opened file: "+f)
+    except(Exception):
+        print("error opening file "+f)
+
+for f in files:
+    try:
+        f1 = xr.open_dataset(f)
+        print("successfully opened file: "+f)
+    except(Exception):
+        print("error opening file "+f)
+
+print("all checks completed.")
