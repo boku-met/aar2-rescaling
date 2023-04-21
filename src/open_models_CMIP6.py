18bce142a7959624b00b27fbc5c480e694745e29
Benedikt Becsi <benedikt.becsi@boku.ac.at>
2023-04-21 16:08:31 +0200

    added src folder and example scripts

---
+++ src/open_models_CMIP6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 15:56:57 2023
+
+@author: bbecsi
+"""
+
+import os
+import glob
+import numpy as np
+import xarray as xr
+
