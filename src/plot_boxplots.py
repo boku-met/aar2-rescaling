33b505d69512abd8b8a05dbde1e6f42fb3adcc6b
Benedikt Becsi <benedikt.becsi@boku.ac.at>
2024-02-14 12:46:51 +0100

    updated plotting scripts

--- src/plot_boxplots.py
+++ src/plot_boxplots.py
@@ -7,10 +7,9 @@ Created on Fri Apr 21 15:56:57 2023
 """
 import os
 import glob
-from joblib import Parallel, delayed
+from multiprocessing.pool import Pool
 import matplotlib.pyplot as plt
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
-import numpy as np
 import xarray as xr
 
 try: 
@@ -45,21 +44,31 @@ def paralell_loop(f):
     time_sample = f1[searchterm_indicators].mean(dim="time", skipna=True)
     area_sample = time_sample.sel(y=slice(ymin, ymax), x=slice(xmin, xmax)).mean(dim=("y","x"), skipna=True)
     return area_sample.values
-par_results = Parallel(n_jobs=12)(delayed(paralell_loop)(f) for f in infiles_cm5)
+
+if __name__ == '__main__':
+    # create and configure the process pool
+    with Pool(14) as pool:
+        # execute tasks, block until all completed
+        par_results = pool.map(paralell_loop, infiles_cm5)
+    # process pool is closed automatically
+
 f1 = xr.open_dataset(infiles_cm5[0])
 vis_data_cm5 = [x.flatten() for x in par_results]
 
-def paralell_loop(f):
-    f1 = xr.open_dataset(f)
-    #anomalies = f1[searchterm_indicators]
-    #ref_period = f1[searchterm_refperiod]
-    #rel_an = (anomalies / ref_period) * 100
-    time_sample = f1[searchterm_indicators].mean(dim="time", skipna=True)
-    area_sample = time_sample.sel(y=slice(ymin, ymax), x=slice(xmin, xmax)).mean(dim=("y","x"), skipna=True)
-    return area_sample.values
-par_results = Parallel(n_jobs=12)(delayed(paralell_loop)(f) for f in infiles_cm6)
+if __name__ == '__main__':
+    # create and configure the process pool
+    with Pool(14) as pool:
+        # execute tasks, block until all completed
+        par_results = pool.map(paralell_loop, infiles_cm6)
+    # process pool is closed automatically
+
 f1 = xr.open_dataset(infiles_cm6[0])
 vis_data_cm6 = [x.flatten() for x in par_results]
+
+vis_data = []
+for cm5, oeks in zip(vis_data_cm5, vis_data_cm6):
+    vis_data.append(oeks)
+    vis_data.append(cm5)
 
 fig, axs = plt.subplots(figsize = (5, 4))
 axs.set_ylabel("Change of annual precipitation sum (%)")
@@ -77,16 +86,24 @@ fig.savefig("/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/pr_vienna_b
 
 
 # comparison plots
-fig, axs = plt.subplots(figsize = (12, 6))
-axs.set_ylabel("Number of days in Kysely periods (Vienna, AT)")
+fig, axs = plt.subplots(figsize = (11, 6))
 axs.boxplot(vis_data, positions=[1,2, 3.5, 4.5, 6,7,8.5,9.5], widths=0.8,patch_artist=True,
             medianprops={"color": "white", "linewidth": 0.5},
             boxprops={"facecolor": "C0", "edgecolor": "white",
                           "linewidth": 0.5},
             whiskerprops={"color": "C0", "linewidth": 1.5},
             capprops={"color": "C0", "linewidth": 1.5},
-            flierprops={"color": "green", "linewidth": 1.0}, 
-            labels=["1.5°C(CM5)", "1.5°C(CM6)", "2.0°C(CM5)", "2.0°C(CM6)", "3.0°C(CM5)", "3.0°C(CM6)", "4.0°C(CM5)", "4.0°C(CM6)"])
+            flierprops={"color": "green", "linewidth": 1.0})
 
-axs.set_xlabel("GWLs")
-fig.savefig("/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/kysely_vienna_cm5_cm6_bp.png", dpi=300)
+axs.tick_params(labelsize=14)
+axs.set_ylabel("Change in number of days in Kysely periods (Vienna, AT)", fontsize = 14, labelpad=10)
+axs.set_xlabel("Ensembles grouped after GWL", fontsize = 14, labelpad=15)
+axs.set_xticklabels(["1.5°C(CMIP5)", "1.5°C(CMIP6)", "2.0°C(CMIP5)", "2.0°C(CMIP6)",
+                     "3.0°C(CMIP5)", "3.0°C(CMIP6)", "4.0°C(CMIP5)", "4.0°C(CMIP6)"],
+                    rotation=45, fontsize=12)
+axs.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.7)
+#axs.set(axisbelow=True)
+plt.title("Annual mean temperature anomalies in Austria relative to 1991-2020\nfor OEKS15 and CMIP5 background models at four GWLs", fontsize = 16, pad = 15)
+
+outpath = "/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/kysely_vienna_cm5_cm6_bp.png"
+plt.savefig(outpath,dpi=300, bbox_inches ="tight")
--- src/plot_create_map.py
+++ src/plot_create_map.py
@@ -7,15 +7,12 @@ Created on Fri Apr 21 15:56:57 2023
 """
 import os
 import glob
-import copy
 from joblib import Parallel, delayed
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 import cartopy.crs as ccrs
 import numpy as np
 import xarray as xr
-import helper_functions as hf
 
 try: 
     os.nice(8-os.nice(0)) # set current nice level to 8, if it is lower 
@@ -23,14 +20,16 @@ except: # nice level already above 8
     pass
 
 path_to_indicators_cm5 = "/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/indicators_gwl/CMIP5/"
-lookup_name = "tas_CMI"
-searchterm_indicators = "tas"
-searchterm_refperiod = "tas_reference_period_1991_2020"
+path_to_indicators_cm6 = "/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/indicators_gwl/CMIP6/"
+lookup_name = "very_heavy_precipitation_days_20mm"
+searchterm_indicators = "very_heavy_precipitation_days_20mm"
+searchterm_refperiod = "very_heavy_precipitation_days_20mm_reference_period_1991_2020"
 
 # for defining the point in the time period
 quantile = 0.5
 
 infiles_cm5 = sorted(glob.glob(path_to_indicators_cm5+lookup_name+"*.nc"))
+infiles_cm6 = sorted(glob.glob(path_to_indicators_cm6+lookup_name+"*.nc"))
 
 # if needed, load reference period
 f_ref_period = xr.open_dataset(infiles_cm5[0])
@@ -38,7 +37,7 @@ vis_data_refperiod = f_ref_period[searchterm_refperiod].quantile(0.5, dim="ens",
 
 def paralell_loop(f):
     f1 = xr.open_dataset(f)
-    time_sample = f1[searchterm_indicators].mean(skipna=True, dim="time")
+    time_sample = f1[searchterm_indicators+"_anomalies"].mean(skipna=True, dim="time")
     ensemble_mean = time_sample.quantile(0.5, dim="ens", skipna=True)
     return ensemble_mean.values
 par_results = Parallel(n_jobs=12)(delayed(paralell_loop)(f) for f in infiles_cm5)
@@ -53,20 +52,37 @@ vis_data_cm5 = xr.DataArray(par_results, coords={"GWL":["1.5째C","2.0째C", "3.0�
 #simple quicklook
 g = vis_data_cm5.plot(x = "x", y = "y", col="GWL", col_wrap=2, aspect = 1.9, size = 3, 
                   cmap="Oranges", cbar_kwargs={"label": "Cooling degree days"}, levels = 9)
-plt.savefig("/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/maps_cdd_gwl.png", dpi=300)
+#plt.savefig("/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/maps_cdd_gwl.png", dpi=300)
 
-vis_data_cm5.sel(GWL="4.0째C").plot.hist()
+glolv = "4.0째C"
 
-lvls = 9
-cmap = plt.cm.YlGnBu.resampled(lvls)
+vis_data_cm5.sel(GWL=glolv).plot.hist(label = "4.0")
+vis_data_cm5.sel(GWL="1.5째C").plot.hist(label = "1.5")
+plt.legend()
+vis_data_refperiod.plot.hist()
+
+# create colors for colorbar
+lvls = 12
+cmap = plt.cm.BrBG.resampled(lvls)
 lst = cmap(np.linspace(0,1,lvls))
 lst = np.insert(lst, 0,[0.8,0.8,0.8,1], axis = 0) # insert grey at position 0
 custom_clrs = [mpl.colors.to_hex(x, keep_alpha=True) for x in lst]
-#setting colorbar ticks and labels
+#custom_clrs = custom_clrs[4:]
 
-level = [0, 10, 15, 20, 25, 30, 35, 40, 45]
-custom_ticks = [10, 15, 20, 25, 30, 35, 40, 45]
-tick_labels = ["10", "15", "20", "25", "30", "35", "40", "45"]
+# create colors for reference colorbar
+lvls_refcbar = 9
+cmap_refcbar = plt.cm.YlGnBu.resampled(lvls_refcbar)
+lst = cmap_refcbar(np.linspace(0,1,lvls_refcbar))
+#lst = np.insert(lst, 0,[0.8,0.8,0.8,1], axis = 0) # insert grey at position 0
+custom_clrs_refcbar = [mpl.colors.to_hex(x, keep_alpha=True) for x in lst]
+
+#setting colorbar ticks and labels
+level = [-2,-1, 0 ,1, 2, 3,4, 5]
+custom_ticks = [-1, 0,1, 2, 3,4, 5]
+tick_labels = ["-1","0","1", "2", "3","4", "5"]
+level_refcbar = [0, 5,10,15,20, 25, 30, 35, 40]
+custom_ticks_refcbar = [ 5, 10, 15, 20, 25, 30, 35, 40]
+
 
 # setting map x and y labels
 xtickspacing = 100000
@@ -79,30 +95,55 @@ yticks = np.arange(vis_data_cm5.y.min()-yoffset+ytickspacing, vis_data_cm5.y.max
 # setting projection
 proj = ccrs.epsg(31287)
 
+#calculating values for annotation
+gwls = vis_data_cm5.coords["GWL"].values
+values_cmip5 = {}.fromkeys(gwls, [])
+values_cmip6 = {}.fromkeys(gwls, [])
+
+values_refperiod = (vis_data_refperiod.min().values.round(1), 
+                    vis_data_refperiod.mean(skipna=True).values.round(1), 
+                    vis_data_refperiod.max().values.round(1))
+values_refperiod = [str(x) for x in values_refperiod]
+
+for gwl in gwls: 
+    values_cmip5.update({gwl:(str(vis_data_cm5.sel(GWL=gwl).min().values.round(1)), 
+                    str(vis_data_cm5.sel(GWL=gwl).mean(skipna=True).values.round(1)), 
+                    str(vis_data_cm5.sel(GWL=gwl).max().values.round(1)))})
+    # values_cmip6.append((vis_data_cm6.sel(GWL=lv).min().values.round(1), 
+    #                 vis_data_cm6.sel(GWL=lv).mean(skipna=True).values.round(1), 
+    #                 vis_data_cm6.sel(GWL=lv).max().values.round(1)))
+
 fig, axs = plt.subplots(ncols=2, nrows=2, 
                             subplot_kw=dict(projection=proj), 
                             figsize = (12,7), 
                             layout = 'constrained')
-for gwl, axis in zip(vis_data_cm5.coords["GWL"], axs.flat):
+for gwl, axis in zip(vis_data_cm5.coords["GWL"].values, axs.flat):
     im = vis_data_cm5.sel(GWL=gwl).plot.imshow(ax = axis, add_colorbar = False, colors = custom_clrs, levels = level)
     axis.gridlines(crs = proj, xlocs = xticks, ylocs = yticks)
-    axis.set(yticks = yticks, ylabel = "y", xticks = xticks, xlabel = "x", title = gwl.values)
-fig.suptitle("Heat days", size = 16)
+    axis.set(yticks = yticks, ylabel = "y", xticks = xticks, xlabel = "x", title = "GWL "+gwl)
+    axis.text(180000,450000, "Min: {0}\nMean: {1}\nMax: {2}".format(*values_cmip5[gwl]), 
+                 style='italic', bbox={'facecolor': 'white'})
+    
+fig.suptitle("Change in heavy precipitation days (>= 20 mm)", size = 16, x = 0.5)
 cax, kwar = mpl.colorbar.make_axes(parents=axs, location="right", fraction=0.17, pad = 0.1)
 fig.delaxes(cax)
-cbar = fig.colorbar(mappable=im, ax=cax, label = "Number of days with tmax >= 30째C", spacing = 'uniform', **kwar)
+cbar = fig.colorbar(mappable=im, ax=cax, label = "Change in number of heavy precipitation days", spacing = 'uniform', **kwar)
 cbar.set_ticks(ticks=custom_ticks, labels=tick_labels)
-fig.get_layout_engine().set(h_pad = 0.15, w_pad = 0.15)
-#outpath = "/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/maps_heatwave_intensity_gwl.png"
-#plt.savefig(outpath,dpi=300, bbox_inches ="tight")
+fig.get_layout_engine().set(h_pad = 0.05, w_pad = 0.11)
+outpath = "/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/maps_heavyprecip20mm_gwl_changes.png"
+plt.savefig(outpath,dpi=300, bbox_inches ="tight")
 
 
-# Single plots
+# Single plots 
 fig, axs = plt.subplots(subplot_kw=dict(projection=proj), layout = 'constrained', figsize = (8,4))
-im = vis_data_refperiod.plot.imshow(colors = custom_clrs, levels = level, cbar_kwargs={"label": "Number of heat days", "spacing": 'uniform', "ticks": custom_ticks})
+im = vis_data_refperiod.plot.imshow(colors = custom_clrs_refcbar, levels = level_refcbar, 
+                                    cbar_kwargs={"label": "Number of heavy precipitation days", "spacing": 'uniform', "ticks": custom_ticks_refcbar})
 axs.gridlines(crs = proj, xlocs = xticks, ylocs = yticks)
 axs.set(yticks = yticks, ylabel = "y", xticks = xticks, xlabel = "x", title = "Reference period 1991-2020")
-fig.suptitle("Mean annual number of heat days (tmax >= 30째C)", size = 16)
-fig.get_layout_engine().set(h_pad = 0.15, w_pad = 0.15)
-outpath = "/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/maps_heatdays_gwl_reference_period.png"
+axs.text(180000,450000, "Min: {0}\nMean: {1}\nMax: {2}".format(*values_refperiod), 
+                 style='italic', bbox={'facecolor': 'white'})
+
+fig.suptitle("Mean annual number of heavy precipitation days (>= 20 mm)", size = 16, x = 0.54)
+fig.get_layout_engine().set(h_pad = 0.11, w_pad = 0.11)
+outpath = "/nas/nas5/Projects/AAR2_rescaling/aar2-rescaling/data/plots/maps_heavyprecip20mm_gwl_reference_period.png"
 plt.savefig(outpath,dpi=300, bbox_inches ="tight")
